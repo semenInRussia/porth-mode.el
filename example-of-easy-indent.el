b6d3d342236a8a4223aab9ea773e296e28f3cf78
semenInRussia <hrams205@gmail.com>
2021-12-24 18:01:36 +0300

    DOC add example of implementation JS

--- example-of-easy-indent.el
+++ example-of-easy-indent.el
@@ -1,15 +1,15 @@
-(message "%s" (macroexpand
-               '(indention/define-for-major-mode
-                 joker joker
-                 :one-indent "  "
-                 :rules
-                 (list
-                  (indention/make-rule
-                   :indent-func 'indention/increment-indent-level
-                   :predicate (lambda () (s-contains-p "{" (thing-at-point 'line t)))
-                   :check-on-prev-line)
-                  (indention/make-rule
-                   :indent-func 'indention/decrement-indent-level
-                   :predicate (lambda () (s-contains-p "}" (thing-at-point 'line t)))
-                   :check-on-prev-line)))))
+(indention/define-for-major-mode
+  js js
+  :one-indent "  "
+  :rules
+  (list
+   (indention/make-rule
+    :indent-func 'indention/increment-indent-level
+    :predicate (lambda () (s-contains-p "{" (thing-at-point 'line t)))
+    :check-on-prev-line)
+   (indention/make-rule
+    :indent-func 'indention/decrement-indent-level
+    :predicate (lambda () (s-contains-p "}" (thing-at-point 'line t))))))
+
+
 
